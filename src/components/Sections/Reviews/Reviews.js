3c4e9de1b3a6c718864331c7b92dce6ebdbb64fe
Cuong Ly <cuonglyy@gmail.com>
2023-04-19 15:22:44 -0700

    Added picture and names for each reviewer

--- src/components/Sections/Reviews/Reviews.js
+++ src/components/Sections/Reviews/Reviews.js
@@ -1,6 +1,10 @@
 import React from "react";
 import styles from "./Reviews.module.css";
 import appStyles from "../../../App.module.css";
+import ali from "../../../assets/images/avatar-ali.png";
+import anisha from "../../../assets/images/avatar-anisha.png";
+import richard from "../../../assets/images/avatar-richard.png";
+import shanai from "../../../assets/images/avatar-shanai.png";
 
 const Reviews = () => {
   return (
@@ -8,7 +12,22 @@ const Reviews = () => {
       <h2 className={styles.header}>What they've said</h2>
       <div className={styles.sliderContainer}>
         <div className={styles.slider}>
-          <div className={styles.sliderItem}></div>
+          <div className={styles.sliderItem}>
+            <img src={anisha} alt="Anisha" />
+            <h3 className={styles.name}>Anisha Li</h3>
+          </div>
+          <div className={styles.sliderItem}>
+            <img src={ali} alt="Ali" />
+            <h3 className={styles.name}>Ali Bravo</h3>
+          </div>
+          <div className={styles.sliderItem}>
+            <img src={richard} alt="Richard" />
+            <h3 className={styles.name}>Richard Watts</h3>
+          </div>
+          <div className={styles.sliderItem}>
+            <img src={shanai} alt="Shanai" />
+            <h3 className={styles.name}>Shanai Gough</h3>
+          </div>
         </div>
       </div>
     </section>
